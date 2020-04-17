b8ed1030e1aee5f383d9b52640f1dd567ed998e9
Yunjae-Na <yunjaena@gmail.com>
2020-04-17 23:18:50 +0900

    Add FFT algorithm
    Add FFT algorithm

--- Multiplication/Multiplication/FFT.cpp
+++ Multiplication/Multiplication/FFT.cpp
@@ -1,1 +1,163 @@
 #include "FFT.h"
+
+
+std::string FFT::get_serial_result(std::string s1, std::string s2) {
+	FFT::set_vector(s1, s2);
+	vector<int> result = serial_multiply(v1, v2);
+	return seial_get_result_from_vector(result);
+}
+
+
+std::string FFT::get_parallel_result(std::string s1, std::string s2) {
+	FFT::set_vector(s1, s2);
+	vector<int> result = parallel_multiply(v1, v2);
+	return parallel_get_result_from_vector(result);
+}
+
+void FFT::set_vector(std::string s1, std::string s2) {
+	v1 = string_to_vi(s1);
+	v2 = string_to_vi(s2);
+	reverse(v1.begin(), v1.end());	
+	reverse(v2.begin(), v2.end());
+}
+
+
+
+void FFT::serial_fft(vector<base>& a, bool inv) {
+	int n = (int)a.size();
+	for (int i = 1, j = 0; i < n; i++) {
+		int bit = n >> 1;
+		while (!((j ^= bit) & bit)) bit >>= 1;
+		if (i < j) swap(a[i], a[j]);
+	}
+	for (int i = 1; i < n; i <<= 1) {
+		double x = (inv ? 1 : -1) * M_PI / i;
+		base w = { cos(x), sin(x) };
+		for (int j = 0; j < n; j += i << 1) {
+			base th(1);
+			for (int k = 0; k < i; k++) {
+				base tmp = a[i + j + k] * th;
+				a[i + j + k] = a[j + k] - tmp;
+				a[j + k] += tmp;
+				th *= w;
+			}
+		}
+	}
+	if (inv) {
+		for (int i = 0; i < n; i++) a[i] /= n;
+	}
+}
+vector<int> FFT::serial_multiply(vector<int>& A, vector<int>& B) {
+	vector<base> a(A.begin(), A.end());
+	vector<base> b(B.begin(), B.end());
+	int n = power_of_2_ge_than(max(a.size(), b.size())) * 2;
+
+	a.resize(n);	b.resize(n);
+	serial_fft(a, false);	serial_fft(b, false);
+
+	for (int i = 0; i < n; i++)
+		a[i] *= b[i];
+	serial_fft(a, true);
+
+	vector<int> ret(n);
+	for (int i = 0; i < n; i++)
+		ret[i] = (int)round(a[i].real());
+	return ret;
+}
+
+void FFT::parallel_fft(vector<base>& a, bool inv) {
+	int n = (int)a.size();
+	for (int i = 1, j = 0; i < n; i++) {
+		int bit = n >> 1;
+		while (!((j ^= bit) & bit)) bit >>= 1;
+		if (i < j) swap(a[i], a[j]);
+	}
+
+	for (int i = 1; i < n; i <<= 1) {
+		double x = (inv ? 1 : -1) * M_PI / i;
+		base w = { cos(x), sin(x) };
+		for (int j = 0; j < n; j += i << 1) {
+			base th(1);
+			for (int k = 0; k < i; k++) {
+				base tmp = a[i + j + k] * th;
+				a[i + j + k] = a[j + k] - tmp;
+				a[j + k] += tmp;
+				th *= w;
+			}
+		}
+	}
+	if (inv) {
+		for (int i = 0; i < n; i++) a[i] /= n;
+	}
+}
+vector<int> FFT::parallel_multiply(vector<int>& A, vector<int>& B) {
+	vector<base> a(A.begin(), A.end());
+	vector<base> b(B.begin(), B.end());
+	int n = power_of_2_ge_than(max(a.size(), b.size())) * 2;
+
+	a.resize(n);	b.resize(n);
+	parallel_fft(a, false);	parallel_fft(b, false);
+	#pragma omp parallel for
+	for (int i = 0; i < n; i++) {
+		a[i] *= b[i];
+	}
+	parallel_fft(a, true);
+
+	vector<int> ret(n);
+	#pragma omp parallel for
+	for (int i = 0; i < n; i++)
+		ret[i] = (int)round(a[i].real());
+	return ret;
+}
+
+std::string FFT::seial_get_result_from_vector(vector<int> &ret) {
+	int i = 0;
+	string s = "";
+	while (i < ret.size()) {
+		if (ret[i] >= 10) {
+			if (i == ret.size() - 1)
+				ret.push_back(ret[i] / 10);
+			else
+				ret[i + 1] += ret[i] / 10;
+			ret[i] %= 10;
+		}
+		++i;
+	}
+
+	reverse(ret.begin(), ret.end());
+
+	bool start = false;
+	for (auto elem : ret) {
+		if (elem)start = true;
+		if (start)s += to_string(elem);
+	}
+	if (!start)s += "0";
+
+	return s;
+}
+
+std::string FFT::parallel_get_result_from_vector(vector<int> &ret) {
+	int i = 0;
+	string s = "";
+	while (i < ret.size()) {
+		if (ret[i] >= 10) {
+			if (i == ret.size() - 1)
+				ret.push_back(ret[i] / 10);
+			else
+				ret[i + 1] += ret[i] / 10;
+			ret[i] %= 10;
+		}
+		++i;
+	}
+
+	reverse(ret.begin(), ret.end());
+
+	bool start = false;
+	for (auto elem : ret) {
+		if (elem)start = true;
+		if (start)s += to_string(elem);
+	}
+	if (!start)s += "0";
+
+	return s;
+}
--- Multiplication/Multiplication/FFT.h
+++ Multiplication/Multiplication/FFT.h
@@ -1,7 +1,69 @@
 #pragma once
+#define _USE_MATH_DEFINES
+#define ll long long
+#include <cstdio>
+#include <iostream>
+#include <cmath>
+#include <math.h> 
+#include <stack>
+#include <queue>
+#include <omp.h>
+#include <vector>
+#include <complex>
+#include <sstream>
+#include <iterator>
+#include <algorithm>
 #include "Algorithm.h"
-class FFT : Algorithm
+
+using namespace std;
+
+class FFT : public Algorithm
 {
+private:
+	std::vector<int> v1;
+	std::vector<int> v2;
+	typedef complex<double> base;
+
+	void set_vector(std::string s1, std::string s2);
+
+	void serial_fft(vector<base>& a, bool inv);
+	vector<int> serial_multiply(vector<int>& A, vector<int>& B);
+	std::string seial_get_result_from_vector(vector<int> &ret);
+
+	void parallel_fft(vector<base>& a, bool inv);
+	vector<int> parallel_multiply(vector<int>& A, vector<int>& B);
+	std::string parallel_get_result_from_vector(vector<int>& ret);
+
 	
+	template <typename T>
+	T bit_reverse(T n) {
+		T ret = 0;
+		while (n) {
+			ret <<= 1;
+			ret |= n & 1;
+			n >>= 1;
+		}
+		return ret;
+	}
+
+	template <typename T>
+	T power_of_2_ge_than(T n) {
+		T ret = 1;
+		while (n > ret) ret <<= 1;
+		return ret;
+	}
+
+	vector<int> string_to_vi(string& s) {
+		vector<int> ret(s.size());
+		for (int i = 0; i < s.size(); i++)
+			ret[i] = s[i] - '0';
+		return ret;
+	}
+
+	std::string vector_to_string(vector<int> vec);
+
+public:
+	virtual std::string get_parallel_result(std::string, std::string);
+	virtual std::string get_serial_result(std::string, std::string);
 };
 
